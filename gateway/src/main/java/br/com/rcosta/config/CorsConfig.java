4b278bb4a4b926554648a8bf694f55844aba8427
Rafael Costa <raf.costa29@gmail.com>
2024-12-26 22:00:18 -0300

    Adicionando CORS 2

---
+++ gateway/src/main/java/br/com/rcosta/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package br.com.rcosta.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // Permitir a origem do Angular
+        config.addAllowedMethod("*"); // Permitir todos os métodos (GET, POST, etc.)
+        config.addAllowedHeader("*"); // Permitir todos os headers
+        config.setAllowCredentials(true); // Permitir cookies ou credenciais
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Aplicar a todas as rotas
+
+        return new CorsWebFilter(source);
+    }
+}
