591189a3b1e9f9103c5dc5a41db4a3972878352f
Rafael Costa <raf.costa29@gmail.com>
2024-12-27 11:52:56 -0300

    Adicionando CORS 9

--- microservices/api-account/src/main/java/br/com/rcosta/account/config/CorsConfig.java
+++ microservices/api-account/src/main/java/br/com/rcosta/account/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("") // Permitir chamadas do Angular
+                .allowedOrigins("*") // Permitir chamadas do Angular
                 .allowedMethods("*") // Permitir todos os métodos HTTP
                 .allowedHeaders("*") // Permitir todos os headers
                 .allowCredentials(true); // Permitir credenciais, se necessário
