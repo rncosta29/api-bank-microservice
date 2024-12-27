e391573bc6c76d813ed210cbf46df7613d5f50bc
Rafael Costa <raf.costa29@gmail.com>
2024-12-27 12:15:49 -0300

    Removendo CORS 1

--- gateway/src/main/java/br/com/rcosta/filter/CustomCorsFilter.java
+++ gateway/src/main/java/br/com/rcosta/filter/CustomCorsFilter.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
@@ -14,16 +16,26 @@ public class CustomCorsFilter implements GlobalFilter, Ordered {
 
 	@Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-        exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
-        exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
-        exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        HttpHeaders headers = exchange.getResponse().getHeaders();
+        
+        // Adiciona cabeçalhos de CORS na resposta
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:4200"); // Origem permitida
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
 
+        // Verifica se é uma preflight request (OPTIONS)
+        if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
+            exchange.getResponse().setStatusCode(HttpStatus.OK); // Responde OK para preflight
+            return Mono.empty();
+        }
+
+        // Continua a execução para outras requisições
         return chain.filter(exchange);
     }
 
     @Override
     public int getOrder() {
-        return Ordered.HIGHEST_PRECEDENCE;
+        return Ordered.HIGHEST_PRECEDENCE; // Alta prioridade para o filtro ser executado antes dos outros
     }
 }
