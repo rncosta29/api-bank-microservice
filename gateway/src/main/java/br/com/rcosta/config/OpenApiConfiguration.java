93c26956b1a32d7c29e8512e99984045bf4215d9
Rafael Costa <raf.costa29@gmail.com>
2024-12-27 14:11:33 -0300

    Removendo CORS 5

--- gateway/src/main/java/br/com/rcosta/config/OpenApiConfiguration.java
+++ gateway/src/main/java/br/com/rcosta/config/OpenApiConfiguration.java
@@ -13,29 +13,29 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfiguration {
 
 	@Bean
-    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters config, RouteDefinitionLocator locator) {
-        var definitions = locator.getRouteDefinitions().collectList().block();
-        List<GroupedOpenApi> groupedOpenApis = new ArrayList<>();
+	public List<GroupedOpenApi> apis(SwaggerUiConfigParameters config, RouteDefinitionLocator locator) {
+	    var definitions = locator.getRouteDefinitions().collectList().block();
+	    List<GroupedOpenApi> groupedOpenApis = new ArrayList<>();
 
-        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*"))
-            .forEach(routeDefinition -> {
-                String name = routeDefinition.getId();
-                config.addGroup(name);  // Adiciona o grupo ao Swagger UI
+	    definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*"))
+	        .forEach(routeDefinition -> {
+	            String name = routeDefinition.getId();
+	            config.addGroup(name);  // Adiciona o grupo ao Swagger UI
 
-                // O caminho para o Swagger dos microserviços
-                String swaggerUrl = "/" + name + "/v3/api-docs";
+	            // O caminho para o Swagger dos microserviços
+	            String swaggerUrl = "/v3/api-docs"; // Alterado para corrigir a URL
 
-                // Configuração do Swagger UI para o microserviço
-                GroupedOpenApi groupedOpenApi = GroupedOpenApi.builder()
-                    .pathsToMatch("/" + name + "/**")
-                    .group(name)
-                    .addOpenApiCustomizer(openApi -> openApi.setServers(
-                        List.of(new io.swagger.v3.oas.models.servers.Server().url(swaggerUrl))))
-                    .build();
+	            // Configuração do Swagger UI para o microserviço
+	            GroupedOpenApi groupedOpenApi = GroupedOpenApi.builder()
+	                .pathsToMatch("/" + name + "/**")
+	                .group(name)
+	                .addOpenApiCustomizer(openApi -> openApi.setServers(
+	                    List.of(new io.swagger.v3.oas.models.servers.Server().url(swaggerUrl)))).build();
 
-                groupedOpenApis.add(groupedOpenApi);
-            });
+	            groupedOpenApis.add(groupedOpenApi);
+	        });
 
-        return groupedOpenApis;
-    }
+	    return groupedOpenApis;
+	}
+
 }
